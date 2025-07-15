0c3c12783dd080f220b8395e2ed759e8f1ac4971
wilfred11 <wilfredvancasteren@hotmail.com>
2025-07-15 11:14:54 +0200

    cov formula

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from statsmodels.sandbox.regression.gmm import IV2SLS
 import os, sys
 from dowhy import CausalModel
 import matplotlib.image as mpimg
-
+from sympy import symbols, diff
 from data import get_data
 
 np.random.seed(1)
@@ -47,22 +47,58 @@ if do==1:
 if do==2:
         df = get_data()
         res_v_e=stats.linregress(df["voucher"], df["education"])
-        res_e_i=stats.linregress(df["education"],df["income"])
+        res_v_i=stats.linregress(df["voucher"],df["income"])
+        res_e_i = stats.linregress(df["education"], df["income"])
 
         plt.scatter(df["voucher"], df["education"])
-        plt.plot(df["voucher"], res_v_e.intercept + res_v_e.slope * df['voucher'], 'r', label='fitted line')
+        plt.plot(df["voucher"], res_v_e.intercept + res_v_e.slope * df['voucher'], 'r', label='voucher/education fitted line')
+        plt.xlabel("voucher")
+        plt.ylabel("education")
         plt.legend()
-        plt.show()
+        plt.savefig("out/v_e")
+        #plt.show()
+        plt.clf()
+
+
+        plt.scatter(df["voucher"], df["income"])
+        plt.plot(df["voucher"], res_v_i.intercept + res_v_i.slope * df['voucher'], 'r', label='voucher/income fitted line')
+        plt.xlabel("voucher")
+        plt.ylabel("income")
+        plt.legend()
+        plt.savefig("out/v_i")
+        plt.clf()
 
         plt.scatter(df["education"], df["income"])
-        plt.plot(df["education"], res_e_i.intercept + res_e_i.slope * df['education'], 'r', label='fitted line')
+        plt.plot(df["education"], res_e_i.intercept + res_e_i.slope * df['education'], 'r',
+                 label='voucher/income fitted line')
+        plt.xlabel("education")
+        plt.ylabel("income")
         plt.legend()
-        plt.show()
+        plt.savefig("out/e_i")
+        #plt.show()
 
         cov_v_e = df['voucher'].cov(df['education'])
         cov_v_i = df['voucher'].cov(df['income'])
-        print("cov")
+        print("estimate= cov('voucher',income')/cov('voucher',education')")
         print(cov_v_i/cov_v_e)
+
+if do==3:
+        df = get_data()
+        res_v_e = stats.linregress(df["voucher"], df["education"])
+        res_v_i = stats.linregress(df["voucher"], df["income"])
+
+        voucher = symbols('voucher', real=True)
+        f_v_e = res_v_e.intercept + (res_v_e.slope * voucher)
+        d_v_e = diff(f_v_e, voucher)
+        #print(d)
+
+        f_v_i = res_v_i.intercept + (res_v_i.slope * voucher)
+        d_v_i = diff(f_v_i, voucher)
+         #print(d2)
+
+        print(d_v_i / d_v_e)
+
+
 
 
 
